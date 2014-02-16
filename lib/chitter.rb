65c83e7f911969614bff1aab3556d0343ec63f18
James Hunter <james.hunter@me.com>
2014-02-16 09:42:47 +0000

    Condensed controller calls

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -5,26 +5,22 @@ require 'sinatra/base'
 
 require_relative 'db_config'
 
-require_relative 'controllers/base'
-require_relative 'controllers/peeps'
-require_relative 'controllers/makers'
-require_relative 'controllers/login'
+Dir.glob(File.join(File.dirname(__FILE__), 'controllers', '*.rb'), &method(:require))
 
 class Chitter < Sinatra::Base
   set :public_folder, File.join(File.dirname(__FILE__), '../public')
 
-  get('/makers/new') { MakersController.call(env) }
-  post('/makers')    { MakersController.call(env) }
+  get('/makers/new')   { MakersController.call(env) }
+  post('/makers')      { MakersController.call(env) }
 
-  get('/')          { PeepsController.call(env) }
-  get('/peeps/new') { PeepsController.call(env) }
-  post('/peeps')    { PeepsController.call(env) }
-  get('/peeps/:id') { PeepsController.call(env) }
+  get('/')             { PeepsController.call(env) }
+  get('/peeps/new')    { PeepsController.call(env) }
+  post('/peeps')       { PeepsController.call(env) }
+  get('/peeps/:id')    { PeepsController.call(env) }
 
   get('/sessions/new') { LoginController.call(env) }
   post('/sessions')    { LoginController.call(env) }
   delete('/sessions')  { LoginController.call(env) }
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
