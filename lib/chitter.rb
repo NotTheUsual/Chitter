e3994732271b751f6e3e31351b1ca7b778384cf3
James Hunter <james.hunter@me.com>
2014-02-16 10:10:42 +0000

    Small fixes

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -13,14 +13,14 @@ class Chitter < Sinatra::Base
   get('/makers/new')   { MakersController.call(env) }
   post('/makers')      { MakersController.call(env) }
 
-  get('/')             { PeepsController.call(env) }
-  get('/peeps/new')    { PeepsController.call(env) }
-  post('/peeps')       { PeepsController.call(env) }
-  get('/peeps/:id')    { PeepsController.call(env) }
+  get('/')             { PeepsController.call(env)  }
+  get('/peeps/new')    { PeepsController.call(env)  }
+  post('/peeps')       { PeepsController.call(env)  }
+  get('/peeps/:id')    { PeepsController.call(env)  }
 
-  get('/sessions/new') { LoginController.call(env) }
-  post('/sessions')    { LoginController.call(env) }
-  delete('/sessions')  { LoginController.call(env) }
+  get('/sessions/new') { LoginController.call(env)  }
+  post('/sessions')    { LoginController.call(env)  }
+  delete('/sessions')  { LoginController.call(env)  }
 
   # start the server if ruby file executed directly
   run! if app_file == $0
