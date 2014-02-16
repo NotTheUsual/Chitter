767fc1863ebb9db56d0a2c16d4c38c2483ec74b5
James Hunter <james.hunter@me.com>
2014-02-16 22:32:51 +0000

    Added comment

--- lib/db_config.rb
+++ lib/db_config.rb
@@ -1,5 +1,6 @@
 env = ENV['RACK_ENV'] || "development"
 
+# ENV['DATABASE_URL'] is for Heroku
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 Dir.glob(File.join(File.dirname(__FILE__), 'models', '*.rb'), &method(:require))
