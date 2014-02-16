2de8bc3c1565cd0544f0050f3e1ddb1e206d103c
James Hunter <james.hunter@me.com>
2014-02-16 22:35:59 +0000

    Hard-coded heroku databse

--- lib/db_config.rb
+++ lib/db_config.rb
@@ -2,7 +2,7 @@ env = ENV['RACK_ENV'] || "development"
 
 # ENV['DATABASE_URL'] is for Heroku
 #  || "postgres://localhost/bookmark_manager_#{env}"
-DataMapper.setup(:default, ENV['DATABASE_URL'])
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://cdfdljcxdxfeoj:EoumGPuhYfU5qsDlRj1q4PzCsu@ec2-54-204-36-244.compute-1.amazonaws.com:5432/d19oe3hvks87i8')
 
 Dir.glob(File.join(File.dirname(__FILE__), 'models', '*.rb'), &method(:require))
 
