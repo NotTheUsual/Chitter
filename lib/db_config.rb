e81caca995e2035d10022789de0516f0e32146ea
James Hunter <james.hunter@me.com>
2014-02-16 22:33:44 +0000

    removed localhost as a test

--- lib/db_config.rb
+++ lib/db_config.rb
@@ -1,7 +1,8 @@
 env = ENV['RACK_ENV'] || "development"
 
 # ENV['DATABASE_URL'] is for Heroku
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
+#  || "postgres://localhost/bookmark_manager_#{env}"
+DataMapper.setup(:default, ENV['DATABASE_URL'])
 
 Dir.glob(File.join(File.dirname(__FILE__), 'models', '*.rb'), &method(:require))
 
