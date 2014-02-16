cefbe87c8af9bdb27feff8cdf853fcb90c571997
James Hunter <james.hunter@me.com>
2014-02-16 22:28:16 +0000

    Fiddled with the DataMapper setup

--- lib/db_config.rb
+++ lib/db_config.rb
@@ -1,6 +1,6 @@
 env = ENV['RACK_ENV'] || "development"
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 Dir.glob(File.join(File.dirname(__FILE__), 'models', '*.rb'), &method(:require))
 
