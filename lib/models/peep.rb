775cdaedf205bf7c48d2e32822fa79f6b9c185a4
James Hunter <james.hunter@me.com>
2014-02-17 13:36:05 +0000

    Small cahnges

--- lib/models/peep.rb
+++ lib/models/peep.rb
@@ -1,15 +1,4 @@
 class Peep
-  # class Link
-  #   include DataMapper::Resource
-
-  #   storage_names[:default] = 'peep_links'
-
-  #   # the person who is following someone (follower)
-  #   belongs_to :reply, 'Peep', :key => true
-
-  #   # the person who is followed by someone (followed)
-  #   belongs_to :parent, 'Peep', :key => true
-  # end
 
 	include DataMapper::Resource
 
@@ -21,26 +10,6 @@ class Peep
 
   has n, :replies, self, :child_key => [ :original_id ]
   belongs_to :original,  self, :required => false #Top level comments have none.
-  
-  # # In this relationship, the person is the follower
-  # has n, :links_to_parents, 'Peep::Link', :child_key => [:reply_id]
-
-  # # In this relationship, the person is the one followed by someone
-  # has n, :links_to_replies, 'Peep::Link', :child_key => [:parent_id]
-
-  # has n, :parents, self,
-  #        :through => :links_to_parents, # The person is a follower
-  #        :via     => :parent
-
-  # has n, :replies, self,
-  #        :through => :links_to_replies, # The person is followed by someone
-  #        :via     => :reply
-
-  # def reply_to(others)
-  #   parents.concat(Array(others))
-  #   save
-  #   self
-  # end
 
   def short_time
   	time.strftime "%b %-d"
